70c2d34f7770f45f7bb9308366ea8b5cf169e3dc
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-12 02:57:17 +0530

    Add User tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   
   validates :name, presence: true, length: { maximum: 150 }
   validates :email, presence: true, length: { maximum: 150 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  
   validates :password, presence: true, length: { in: 6..20 }
   
   has_secure_password
