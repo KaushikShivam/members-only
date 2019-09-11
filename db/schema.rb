d4c56dd2e6c57ba3488ff4f3d800cdb871850b43
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-12 01:54:47 +0530

    Add password migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190911200911) do
+ActiveRecord::Schema.define(version: 20190911201919) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
