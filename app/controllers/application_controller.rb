5c6dc333d848fb1edd7e9fdac15d8a7a24282cf6
fatima ahmed <fatymah001@gmail.com>
2019-09-13 00:40:04 +0300

    Add Log in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def log_in(user)
+    session[:user_id]=user.id
+    new_token=User.create_token
+    cookies.permanent[:remember_token]=new_token
+    cookies.permanent.signed[:user_id]=user.id
+    user.update_attribute(:remember_digest, User.digest(new_token))
+  end
+
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      redirect_to @user
+     
+    else
+      flash[:error]="Wrong email/password"
+      render 'new'
+    end
   end
 
   def destroy
