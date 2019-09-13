b0eb155f94b6b9f586d831b1f431d68f3a536ee4
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-13 23:02:57 +0530

    Fix autho identity

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true, length: {maximum: 155}
-  validates :body, presence: true, length: { in: 10..1000}
+  validates :title, presence: true, length: { maximum: 155 }
+  validates :body, presence: true, length: { in: 10..1000 }
 end
