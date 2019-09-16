50249ec841a18b99669dbf47fa7235376a835856
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-16 21:08:04 +0530

    Implement posts display by date

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
   belongs_to :user
   validates :title, presence: true, length: { maximum: 155 }
   validates :body, presence: true, length: { in: 10..1000 }
